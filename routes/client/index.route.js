0befd6f748f061c0d7a6f3758d70e781a5dc7e82
Enanan <enanan@Shinonome.lan>
2025-11-18 22:29:00 +0700

    update folder controllers - coi nhu xong mo hinh MVC

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,9 +1,7 @@
 const productsRoutes = require("./products.route");
+const homeRoutes = require("./home.route");
 
 module.exports = (app) => {
-  app.get("/", (req, res) => {
-    res.render("client/pages/home/index");
-  });
-
+  app.use("/", homeRoutes);
   app.use("/products", productsRoutes);
 };
--- routes/client/products.route.js
+++ routes/client/products.route.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const productController = require("../../controllers/client/product.controller");
 
-router.get("/", (req, res) => {
-  res.render("client/pages/products/index");
-});
+router.get("/", productController.index);
 
 module.exports = router;
