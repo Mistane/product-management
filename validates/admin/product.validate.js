29b6dcbedf79d979d4025e52a058f13ca84e3fbc
Enanan <enanan@Shinonome.lan>
2025-11-27 15:51:39 +0700

    THem tinh nang validate

---
+++ validates/admin/product.validate.js
@@ -0,0 +1,11 @@
+const systemConfig = require("../.././config/system");
+
+module.exports.createPost = (req, res, next) => {
+  if (req.body.title === "") {
+    req.flash("error", "Vui long dien tieu de san pham !");
+    res.redirect(`${systemConfig.prefixAdmin}/products`);
+    return;
+  }
+  console.log("ok");
+  next();
+};
