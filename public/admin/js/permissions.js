f00b21f5083c65f62311b662429da3cd2b6fd4ab
Enanan <enanan@Shinonome.lan>
2025-12-08 21:00:25 +0700

    Vai thieu {} ma van chay duoc a lmao

--- public/admin/js/permissions.js
+++ public/admin/js/permissions.js
@@ -4,18 +4,19 @@ if (data) {
   const records = JSON.parse(data.getAttribute("data"));
   const rows = document.querySelectorAll("[data-name]");
   rows.forEach((row) => {
-    if (row.getAttribute("data-name") != "");
-    row.querySelectorAll("input").forEach((input, index) => {
-      const idx = index;
-      records.forEach((data, index) => {
-        if (
-          data.permissions.indexOf(row.getAttribute("data-name")) != -1 &&
-          idx == index
-        ) {
-          input.checked = true;
-        }
+    if (row.getAttribute("data-name") != "") {
+      row.querySelectorAll("input").forEach((input, index) => {
+        const idx = index;
+        records.forEach((data, index) => {
+          if (
+            data.permissions.indexOf(row.getAttribute("data-name")) != -1 &&
+            idx == index
+          ) {
+            input.checked = true;
+          }
+        });
       });
-    });
+    }
   });
 }
 //----------------perm edit----------------------------------
